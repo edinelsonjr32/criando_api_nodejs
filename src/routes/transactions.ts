b0ca4ab244acc4efbbec302a27f8686131b8c882
edinelsonjr32 <edinelsonjr.ufopa@gmail.com>
2025-03-28 14:30:05 -0300

    Listando todas as transações e buscando uma transação individual

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import crypto from 'node:crypto'
 import { z } from 'zod'
+import { request } from 'node:http'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.post('/', async (request, response) => {
@@ -24,5 +25,23 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     // HTTP codes ()
     return response.status(201).send()
+  })
+
+  // listagem de transações
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
   })
 }
